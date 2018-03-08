4d871301caa237882c748166da50aa8bfba1eadd
lfarrell <lfarrell@email.unc.edu>
2018-03-08 09:30:51 -0500

    Add basic server side processing for works table

--- app/controllers/hyrax/dashboard_controller.rb
+++ app/controllers/hyrax/dashboard_controller.rb
@@ -17,5 +17,24 @@ module Hyrax
         render 'show_user'
       end
     end
+
+    def repository_growth
+      return unless can? :read, :admin_dashboard
+      @repo_growth = Hyrax::Admin::RepositoryGrowthPresenter.new(params[:time_period])
+      render json: @repo_growth
+    end
+
+    def repository_object_counts
+      return unless can? :read, :admin_dashboard
+      @repo_objects = Hyrax::Admin::RepositoryObjectPresenter.new(params[:type_value])
+      render json: @repo_objects
+    end
+
+    def update_works_list
+      if can? :read, :admin_dashboard
+        @work_rows = Hyrax::WorksCountService.new(self, Hyrax::WorksSearchBuilder, nil, params).search_results_with_work_count(:read)
+        render json: @work_rows
+      end
+    end
   end
 end
